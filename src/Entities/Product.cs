212421ab8748f9f47a8028b7caebfac528d47a6d
Frederik Bertelsen <frbb@itu.dk>
2026-01-02 22:30:36 +0100

    fix: make Product price settable

--- src/Entities/Product.cs
+++ src/Entities/Product.cs
@@ -3,5 +3,5 @@ namespace EfCoreApiTemplate.src.Entities;
 public class Product : BaseEntity
 {
     public required string Name { get; init; }
-    public required float Price { get; init; }
+    public required float Price { get; set; }
 }
