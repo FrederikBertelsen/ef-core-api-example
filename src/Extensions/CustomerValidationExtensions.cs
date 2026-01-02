b1fae2c6c01ee49da4e553f1252702acede78f5f
Frederik Bertelsen <frbb@itu.dk>
2026-01-02 21:26:28 +0100

    fix: standardize casing for validation exception messages for Customer

--- src/Extensions/CustomerValidationExtensions.cs
+++ src/Extensions/CustomerValidationExtensions.cs
@@ -8,27 +8,27 @@ public static class CustomerValidationExtensions
     {
         ArgumentNullException.ThrowIfNull(createCustomerDto);
         if (string.IsNullOrWhiteSpace(createCustomerDto.FirstName))
-            throw new ArgumentException("The Customer is missing a first name");
+            throw new ArgumentException("The Customer is missing a FirstName");
         if (string.IsNullOrWhiteSpace(createCustomerDto.LastName))
-            throw new ArgumentException("The Customer is missing a last name");
+            throw new ArgumentException("The Customer is missing a LastName");
         if (string.IsNullOrWhiteSpace(createCustomerDto.Email))
-            throw new ArgumentException("The Customer is missing an email");
+            throw new ArgumentException("The Customer is missing an Email");
         if (string.IsNullOrWhiteSpace(createCustomerDto.Address))
-            throw new ArgumentException("The Customer is missing an address");
+            throw new ArgumentException("The Customer is missing an Address");
     }
 
     public static void ValidateOrThrow(this CustomerDto CustomerDto)
     {
         ArgumentNullException.ThrowIfNull(CustomerDto);
         if (CustomerDto.Id == Guid.Empty)
-            throw new ArgumentException("The Customer is missing an Id");
+            throw new ArgumentException("The Customer is missing an ID");
         if (string.IsNullOrWhiteSpace(CustomerDto.FirstName))
-            throw new ArgumentException("The Customer is missing a first name");
+            throw new ArgumentException("The Customer is missing a FirstName");
         if (string.IsNullOrWhiteSpace(CustomerDto.LastName))
-            throw new ArgumentException("The Customer is missing a last name");
+            throw new ArgumentException("The Customer is missing a LastName");
         if (string.IsNullOrWhiteSpace(CustomerDto.Email))
-            throw new ArgumentException("The Customer is missing an email");
+            throw new ArgumentException("The Customer is missing an Email");
         if (string.IsNullOrWhiteSpace(CustomerDto.Address))
-            throw new ArgumentException("The Customer is missing an address");
+            throw new ArgumentException("The Customer is missing an Address");
     }
 }
