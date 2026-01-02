a66c97531dcb086520d64edbf4887d2988058108
Frederik Bertelsen <frbb@itu.dk>
2026-01-02 20:17:36 +0100

    add: validation extensions for Customer DTOs

---
+++ src/Extensions/CustomerValidationExtensions.cs
@@ -0,0 +1,32 @@
+using EfCoreApiTemplate.src.DTOs;
+
+namespace EfCoreApiTemplate.src.Extensions;
+
+public static class CustomerValidationExtensions
+{
+    public static void ValidateOrThrow(this CreateCustomerDto createCustomerDto)
+    {
+        ArgumentNullException.ThrowIfNull(createCustomerDto);
+        if (string.IsNullOrWhiteSpace(createCustomerDto.FirstName))
+            throw new ArgumentException("The Customer is missing a first name");
+        if (string.IsNullOrWhiteSpace(createCustomerDto.LastName))
+            throw new ArgumentException("The Customer is missing a last name");
+        if (string.IsNullOrWhiteSpace(createCustomerDto.Email))
+            throw new ArgumentException("The Customer is missing an email");
+        if (string.IsNullOrWhiteSpace(createCustomerDto.Address))
+            throw new ArgumentException("The Customer is missing an address");
+    }
+
+    public static void ValidateOrThrow(this CustomerDto CustomerDto)
+    {
+        ArgumentNullException.ThrowIfNull(CustomerDto);
+        if (string.IsNullOrWhiteSpace(CustomerDto.FirstName))
+            throw new ArgumentException("The Customer is missing a first name");
+        if (string.IsNullOrWhiteSpace(CustomerDto.LastName))
+            throw new ArgumentException("The Customer is missing a last name");
+        if (string.IsNullOrWhiteSpace(CustomerDto.Email))
+            throw new ArgumentException("The Customer is missing an email");
+        if (string.IsNullOrWhiteSpace(CustomerDto.Address))
+            throw new ArgumentException("The Customer is missing an address");
+    }
+}
