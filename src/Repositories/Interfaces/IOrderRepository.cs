ab99c1be1a25f495cdc5228a43568a91bd827631
Frederik Bertelsen <frbb@itu.dk>
2026-01-03 23:23:51 +0100

    fix: make IOrderRepository use the new OrderItem class

--- src/Repositories/Interfaces/IOrderRepository.cs
+++ src/Repositories/Interfaces/IOrderRepository.cs
@@ -5,8 +5,8 @@ namespace EfCoreApiTemplate.src.Repositories.Interfaces;
 public interface IOrderRepository
 {
     public Task<OrderDto> CreateOrder(CreateOrderDto createOrderDto);
-    public Task AddProductsToOrder(Guid orderId, ICollection<Guid> productIds);
-    public Task RemoveProductsFromOrder(Guid orderId, ICollection<Guid> productIds);
+    public Task AddProductsToOrder(Guid orderId, ICollection<OrderItemDto> productsToAdd);
+    public Task RemoveProductsFromOrder(Guid orderId, ICollection<OrderItemDto> productsToRemove);
     public Task MarkOrderAsComplete(Guid orderId);
     public Task DeleteOrder(Guid orderId);
 }
