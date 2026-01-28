8974622efc00594367bd81c28bce450fb4e2bb1c
Frederik Bertelsen <frbb@itu.dk>
2026-01-28 23:02:38 +0100

    fix: forgot to imclude orders on get customer in repository

--- src/Repositories/CustomerRepository.cs
+++ src/Repositories/CustomerRepository.cs
@@ -8,8 +8,8 @@ namespace EfCoreApiExample.src.Repositories;
 public class CustomerRepository(AppDbContext dbContext) : ICustomerRepository
 {
     public async Task<Customer> CreateCustomerAsync(Customer customer) => (await dbContext.Customers.AddAsync(customer)).Entity;
-    public async Task<Customer?> GetCustomerByIdAsync(Guid customerId) => await dbContext.Customers.FindAsync(customerId);
-    public async Task<Customer?> GetCustomerByEmailAsync(string email) => await dbContext.Customers.FirstOrDefaultAsync(c => c.Email == email);
+    public async Task<Customer?> GetCustomerByIdAsync(Guid customerId) => await dbContext.Customers.Include(customer => customer.Orders).FirstOrDefaultAsync(customer => customer.Id == customerId);
+    public async Task<Customer?> GetCustomerByEmailAsync(string email) => await dbContext.Customers.Include(customer => customer.Orders).FirstOrDefaultAsync(c => c.Email == email);
     public Customer UpdateCustomer(Customer customer) => dbContext.Customers.Update(customer).Entity;
     public void DeleteCustomer(Customer customer) => dbContext.Customers.Remove(customer);
     public Task<bool> CustomerExistsByIdAsync(Guid customerId) => dbContext.Customers.AnyAsync(c => c.Id == customerId);
